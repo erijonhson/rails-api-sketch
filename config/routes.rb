1777146c867ea47ea812b08870513ff8b103b325
erijonhson <eri.silva@ccc.ufcg.edu.br>
2017-09-14 04:34:49 +0000

    Desabilitando as rotas do devise temporariamente

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'api_version_constraint'
 
 # TODO add host api.rails-api-sketch.dev to 127.0.1.1 on the OS on which server was deployed 
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
