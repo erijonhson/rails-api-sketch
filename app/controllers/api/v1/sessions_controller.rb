0b099abf46d9e78ff8940f4369d6cecbaf8e58a8
erijonhson <eri.silva@ccc.ufcg.edu.br>
2017-09-14 22:28:50 +0000

    TDD Adicionando destroy action no controle de sessões

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -17,6 +17,7 @@ class Api::V1::SessionsController < ApplicationController
       user = User.find_by(auth_token: params[:id])
       user.generate_authentication_token!
       user.save
+      head :no_content
     rescue
       head :not_found
     end
