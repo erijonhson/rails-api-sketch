fd1246d08319182be6c74d4a3d6c790dc42a7c0a
erijonhson <eri.silva@ccc.ufcg.edu.br>
2017-09-14 06:27:02 +0000

    Chaves de response body agora são Symbols

--- spec/requests/api/v1/users.spec.rb
+++ spec/requests/api/v1/users.spec.rb
@@ -14,8 +14,8 @@ RSpec.describe 'Users API', type: :request do
 
     context 'when the user exists' do
       it 'returns the user' do
-        user_response = JSON.parse(response.body)
-        expect(user_response['id']).to eq(user_id)
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:id]).to eq(user_id)
       end
 
       it 'return status 200' do
@@ -46,8 +46,8 @@ RSpec.describe 'Users API', type: :request do
       end
 
       it 'return json data for the created user' do
-        user_response = JSON.parse(response.body)
-        expect(user_response['email']).to eq(user_params[:email])
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq(user_params[:email])
       end
     end
 
@@ -59,8 +59,8 @@ RSpec.describe 'Users API', type: :request do
       end
 
       it 'return json data for the created user' do
-        user_response = JSON.parse(response.body)
-        expect(user_response).to have_key('erros')
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:erros)
       end
     end
   end
