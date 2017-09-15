80b07e777d395f0f16e9c3398f4eb2ce67f47b13
erijonhson <eri.silva@ccc.ufcg.edu.br>
2017-09-15 06:29:40 +0000

    TDD Adicionando Task#show

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -5,4 +5,9 @@ class Api::V1::TasksController < ApplicationController
     tasks = current_user.tasks
     render json: { tasks: tasks }, status: :ok
   end
+
+  def show
+    task = current_user.tasks.find(params[:id])
+    render json: task, status: :ok
+  end
 end
