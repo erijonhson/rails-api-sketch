6ded860b90e463b36ed40ba975716ed6e7ddda62
erijonhson <eri.silva@ccc.ufcg.edu.br>
2017-09-15 02:23:20 +0000

    Adicionando authenticate_with_token

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -21,4 +21,27 @@ RSpec.describe Authenticable do
       expect(app_controller.current_user).to eq(user)
     end
   end
+
+  describe '#authenticate_with_token!!' do
+    controller do
+      before_action :authenticate_with_token!
+      def restricted_action; end
+    end
+
+    context 'when there is no user logged in' do
+      before do
+        allow(app_controller).to receive(:current_user).and_return(nil)
+        routes.draw { get 'restricted_action' => 'anonymous#restricted_action' }
+        get :restricted_action
+      end
+
+      it 'return status code 401' do
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it 'return the json data for the errors' do
+        expect(json_body).to have_key(:errors)
+      end
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -45,7 +45,7 @@ RSpec.configure do |config|
   # https://github.com/thoughtbot/factory_girl_rails
   # include all methods (include, create, ...)
   config.include FactoryGirl::Syntax::Methods
-  config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
