61988bfb0711f60190d447a4880ba8c54d073944
erijonhson <eri.silva@ccc.ufcg.edu.br>
2017-09-15 06:57:57 +0000

    TDD Adicionando Task#create

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -10,4 +10,19 @@ class Api::V1::TasksController < ApplicationController
     task = current_user.tasks.find(params[:id])
     render json: task, status: :ok
   end
+
+  def create
+    task = current_user.tasks.build(task_params)
+    if task.save
+      render json: task, status: :created
+    else
+      render json: { errors: task.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :deadline, :done)
+  end
 end
