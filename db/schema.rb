0b4c8dde992f4839fb52857001502cf93af6bd1c
erijonhson <eri.silva@ccc.ufcg.edu.br>
2017-09-16 04:52:46 +0000

    Adaptando para deploy no heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,35 +12,38 @@
 
 ActiveRecord::Schema.define(version: 20170915212724) do
 
-  create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
     t.string   "title"
-    t.text     "description", limit: 65535
-    t.boolean  "done",                      default: false
+    t.text     "description"
+    t.boolean  "done",        default: false
     t.datetime "deadline"
     t.integer  "user_id"
-    t.datetime "created_at",                                null: false
-    t.datetime "updated_at",                                null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.index ["user_id"], name: "index_tasks_on_user_id", using: :btree
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                                default: "",      null: false
-    t.string   "encrypted_password",                   default: "",      null: false
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "",      null: false
+    t.string   "encrypted_password",     default: "",      null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                        default: 0,       null: false
+    t.integer  "sign_in_count",          default: 0,       null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                             null: false
-    t.datetime "updated_at",                                             null: false
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
     t.string   "auth_token"
-    t.string   "provider",                             default: "email", null: false
-    t.string   "uid",                                  default: "",      null: false
+    t.string   "provider",               default: "email", null: false
+    t.string   "uid",                    default: "",      null: false
     t.string   "name"
-    t.text     "tokens",                 limit: 65535
+    t.text     "tokens"
     t.index ["auth_token"], name: "index_users_on_auth_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
