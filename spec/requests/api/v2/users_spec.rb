5f8986fba5900a42fb67325dc384883d6916169d
erijonhson <eri.silva@ccc.ufcg.edu.br>
2017-09-16 01:40:37 +0000

    Atualizando v2-api sessions

--- spec/requests/api/v2/users_spec.rb
+++ spec/requests/api/v2/users_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Users API', type: :request do
   let!(:user) { create(:user) }
-  let(:auth_data) { user.create_new_auth_token }
+  let!(:auth_data) { user.create_new_auth_token }
   let(:headers) do
     {
       'Accept' => 'application/vnd.rails-api-sketch.v2',
