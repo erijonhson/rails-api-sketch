8b24638eb25ca907afcc40feeb10db346c6b2929
Eri Jonhson <eri.silva@ccc.ufcg.edu.br>
2017-09-20 12:28:49 -0300

    Atualizando segurança para Users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < Api::V1::BaseController
 
-  before_action :authenticate_with_token!, only: [:update, :destroy]
+  before_action :authenticate_with_token!, only: [:show, :update, :destroy]
   respond_to :json
 
   def show
